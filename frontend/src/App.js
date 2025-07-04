5ac51b727486abbbd60c7318a71e955cf87f1b12
Jarpik <jarpikholdings@gmail.com>
2025-07-04 13:30:27 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import StatleNBAPage from './pages/StatleNBAPage';
+import DraftDuelPage from './pages/DraftDuelPage'; 
 import './styles/index.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/StatleNBA" element={<StatleNBAPage />} />
+          <Route path="/DraftDuel" element={<DraftDuelPage />} /> 
         </Routes>
       </div>
     </Router>
